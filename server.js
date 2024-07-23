1548819e9761f0bbf54195ceb022c5674baaf7de
Yeahyun Kim <insidesy4@gmail.com>
2024-07-24 08:19:17 +0900

    [Add] server.js / package.json 설정

---
+++ server.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const path = require('path');
+const Bundler = require('parcel-bundler');
+
+const app = express();
+  const port = 3000;
+
+// Parcel 번들러 설정
+const bundler = new Bundler(['html/main.html', 'html/signup.html', 'html/home.html', 'html/board.html'], {
+  outDir: './dist', // 번들된 파일들이 저장될 디렉토리
+  watch: true, // 파일 변경 감지
+});
+
+// Parcel 미들웨어 사용
+app.use(bundler.middleware());
+
+// 정적 파일 서빙
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// 기본 루트 경로 요청 처리
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'main.html'));
+});
+
+// /login 경로 요청 처리
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'login.html'));
+});
+
+// /signup 경로 요청 처리
+app.get('/signup', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'signUp.html'));
+});
+
+// /signup 경로 요청 처리
+app.get('/home', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'home.html'));
+});
+
+// /boards/:boardId 경로 요청 처리
+app.get('/boards/:boardId', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'board.html'));
+});
+
+
+// /boards/:boardId 경로 요청 처리
+app.get('/boards/:boardId/decks/:deckId', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'board.html'));
+});
+
+
+// 토큰 유효성 검사 미들웨어
+const checkAuth = (req, res, next) => {
+  const token = req.headers['accsstoken'];
+  console.log(req.headers);
+
+  // 토큰 유효성 검사 로직 (여기서는 예시로 간단하게 처리)
+  if (!token || token !== 'accsstoken') {
+    return res.redirect('/login');
+  }
+  next();
+};
+
+// 특정 경로에 대해서만 토큰 유효성 검사 미들웨어 적용
+app.use(['/protected-route1', '/protected-route2'], checkAuth);
+
+// 다른 모든 경로에 대해서는 404 페이지 반환
+app.use((req, res, next) => {
+  res.status(404).send('페이지를 찾을 수 없습니다.');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
